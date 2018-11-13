f10f6ab6ea7886e2153b3c1242dedcfe9a36d696
JanTry <44120531+WDIjantry@users.noreply.github.com>
2018-11-13 15:27:17 +0100

    Update Dodatkowe/4.19.cpp

--- Dodatkowe/4.19.cpp
+++ Dodatkowe/4.19.cpp
@@ -4,34 +4,48 @@
 
 using namespace std;
 
-const int MAX=100;
+const int N=5;
 
-int iloczyn(int tab[MAX][MAX],double x)
-{
-    double a,b;
-    int q=0;
-    for(int i=0;i<MAX;i++)
-        for(int j=0;j<MAX;j++)
-            for(int k=i;k<MAX;k++)
-                if(k==i)
-                    for(int p=j+1;p<MAX;p++)
-                    {
-                        if(1.0*tab[i][j]/tab[k][p]==x)
-                                q++;
-                    }
-                else
-                    for(int p=0;p<MAX;p++)
-                    {
-                        if(1.0*tab[i][j]/tab[k][p]==x)
-                            q++;
-                    }
-
-    return q;
+int pary(int tab[N][N],double iloczyn){
+    int t[N+4][N+4];
+    int licznik=0;
+    for(int i=2;i<N+2;i++)
+        for(int j=2;j<N+2;j++)
+            t[i][j]=tab[i-2][j-2];
+    for(int i=0;i<N+4;i++){
+        t[0][i]=-1;
+        t[1][i]=-1;
+        t[i][0]=-1;
+        t[i][1]=-1;
+        t[N+3][i]=-1;
+        t[N+2][i]=-1;
+        t[i][N+2]=-1;
+        t[i][N+3]=-1;
+    }
+    for(int i=2;i<N;i++)
+        for(int j=2;j<N+2;j++){
+            if(1.0*t[i][j]/t[i-2][j-1]==iloczyn) licznik++;
+            if(1.0*t[i][j]/t[i-1][j-2]==iloczyn) licznik++;
+            if(1.0*t[i][j]/t[i+2][j-1]==iloczyn) licznik++;
+            if(1.0*t[i][j]/t[i-2][j+1]==iloczyn) licznik++;
+            if(1.0*t[i][j]/t[i-1][j+2]==iloczyn) licznik++;
+            if(1.0*t[i][j]/t[i+2][j+1]==iloczyn) licznik++;
+            if(1.0*t[i][j]/t[i+1][j+2]==iloczyn) licznik++;
+            if(1.0*t[i][j]/t[i+1][j-2]==iloczyn) licznik++;
+        }
+    return licznik;
 }
+
+
+
 
 int main()
 {
-    double x;
-    int a,b;
-    int tab[MAX][MAX];
+    int tab[N][N];
+    double iloczyn;
+    cin>>iloczyn;
+    for(int i=0;i<N;i++)
+        for(int j=0;j<N;j++)
+            cin>>tab[i][j];
+    cout<<pary(tab,iloczyn)<<endl;
 }
