a05826a2be6d7bc3fb3ff205548004fb2687cad3
JanTry <44120531+WDIjantry@users.noreply.github.com>
2018-12-11 17:29:57 +0100

    Update 2016.2017.2b1.cpp

--- Dodatkowe/Test2-prep/2016.2017.2b1.cpp
+++ Dodatkowe/Test2-prep/2016.2017.2b1.cpp
@@ -12,7 +12,10 @@ bool pierwsza(int a){
 
 int sumy(int t1[N], int t2[N], int n=0, int s=0){ //sumy(t1,t2);
     if(n==N){
-        if(pierwsza(s))return 1;
+        if(pierwsza(s)){
+            cout<<s<<" ";
+            return 1;
+        }
         return 0;
     }
     return sumy(t1,t2,n+1,s+t1[n]+t2[n])+sumy(t1,t2,n+1,s+t1[n])+sumy(t1,t2,n+1,s+t2[n]);
