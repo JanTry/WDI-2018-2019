e68332342905300fc05f9f8dbc8d738fc5ed496e
JanTry <44120531+WDIjantry@users.noreply.github.com>
2018-12-11 16:29:02 +0100

    Update 2016.2017.2a1.cpp

--- Dodatkowe/Test2-prep/2016.2017.2a1.cpp
+++ Dodatkowe/Test2-prep/2016.2017.2a1.cpp
@@ -2,33 +2,23 @@
 
 using namespace std;
 
-//Zadanie 1 z grupy a z 2 kartkówki z WDI semestrze zimowym 2016/2017.
-
-int NWD(int a, int b){ //Klasyczny Euklides. NWD(a,b);
+int NWD(int a, int b){
     while(b!=0){
-        int c=b;    //alternatywnie-> while(b!=0)swap(a%=b,b);
+        int c=b;
         b=a%b;
         a=c;
     }
     return a;
 }
-/*Alternatywnie
-int NWD(int a, int b){
-    while(b!=0)swap(a%=b,b);
-    return a;
-}
-*/
-
-int pary(int N, int a=0, int b=0){ //pary(N); -> argumenty definiowane "int a=0" ustawiaja domyslna wartosc "a" jako 0. Tzn, ze jezeli "a" nie zostanie podane, to zostanie przyjêta wartoœæ 0
-    if(N==0){ //Gdy jest to ostatnie wywo³anie, a zatem podzia³ na  a i b jest juz gotowy
-        if(NWD(a,b)==1){ //Jezeli NWD==1 <=> jezeli sa wzglednie pierwsze
-            cout<<a<<" "<<b<<endl; //wypisujemy je
-            return 1; //zwracamy 1, skoro owa para sie znalazla
+int pary(int N, int a=0, int b=0){
+    if(N==0){
+        if(NWD(a,b)==1){
+            cout<<a<<" "<<b<<endl;
+            return 1;
         }
-        else return 0; // zwracamy 0, skoro nie sa wzglednie pierwsze
+        else return 0;
     }
-    return pary(N/10, a*10 +(N%10), b) + pary(N/10, a, b*10 +(N%10)); //Wywolujemy kolejne instancje dodajac cyfre do a, lub do b i zmienjszajac o nia N.
-} //Zwracamy w kazdej instancji (poza ostatnia) sume uzbieranych par.
-//tzn- pary(....) jest suma zer i jedynek otrzymanych w ostatnich instancjach (takich ostatnich instancji bedzie 2 do potegi n).
+    return pary(N/10, a*10 +(N%10), b) + pary(N/10, a, b*10 +(N%10));
+}
 
 int main(){}
