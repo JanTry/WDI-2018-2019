e7826b6af7fdb550f40078811b3d83654c0ea834
JanTry <44120531+WDIjantry@users.noreply.github.com>
2018-12-11 17:47:37 +0100

    Delete XD.201617.2b1.cpp

--- Dodatkowe/Test2-prep/XD.201617.2b1.cpp
+++
@@ -1,2 +0,0 @@
-#include<iostream>
-using namespace std;const int N=100;bool pierwsza(int a){if(a==1)return 0;for(int i=2;i*i<=a;i++)if(a%i==0)return 0;return 1;}int sumy(int t1[N],int t2[N],int n=0,int s=0){if(n==N){if(pierwsza(s))return 1;return 0;}return sumy(t1,t2,n+1,s+t1[n]+t2[n])+sumy(t1,t2,n+1,s+t1[n])+sumy(t1,t2,n+1,s+t2[n]);}int main(){}
